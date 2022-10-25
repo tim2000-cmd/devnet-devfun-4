e3ec8be9f351e54f52d5b389cacd947f505a1071
tim2000-cmd <tim.james@scc.com>
2022-10-25 12:30:35 +0100

    More Updates

--- Test-7.py
+++ Test-7.py
@@ -1,5 +1,5 @@
+from statistics import quantiles
 from tkinter.font import names
-
 
 for t in range(5):
     print (t)
@@ -7,3 +7,12 @@ for t in range(5):
 names = ["Tim", "John", "James"]
 for name in names:
     print (name)
+#
+dc_inventory = {"switches":5, "routers":10, "firewalls":9}
+for device in dc_inventory:
+    print (device)
+#
+#
+for device, quantity in dc_inventory.items():
+    print("You have {}{}in the SW data center.".format(device, quantity))
+
